119aa816d48d1d98c304cede7c0a9ef847424207
OTeeEnabor <tobienabor@gmail.com>
2024-11-12 11:43:06 +0200

    made change to storages to be used with Django

--- aws_backend_project/settings.py
+++ aws_backend_project/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "opendelta_store",
     "rest_framework",
     "drf_yasg",
+    "storages",
 ]
 
 MIDDLEWARE = [
@@ -146,13 +147,30 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = json.loads(os.getenv("EMAIL_USE_TLS").lower())
 EMAIL_USE_SSL = json.loads(os.getenv("EMAIL_USE_SSL").lower())
 
+# AWS Configuration
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+# AWS Basic Storage Configuration for Amazon S3
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+AWS_S3_SIGNATURE_NAME = os.getenv("AWS_S3_SIGNATURE_NAME")
+AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME")
+AWS_S3_FILE_OVERWRITE = json.loads(os.getenv("AWS_S3_FILE_OVERWRITE").lower())
+AWS_DEFAULT_ACL = json.loads(os.getenv("AWS_DEFAULT_ACL"))
+AWS_S3_VERITY = json.loads(os.getenv("AWS_S3_VERITY").lower())
+# DEFAULT_FILE_STORAGE=os.getenv("DEFAULT_FILE_STORAGE")
 
-AWS_ACCESS_KEY_ID =os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY=os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME=os.getenv("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_SIGNATURE_NAME=os.getenv("AWS_S3_SIGNATURE_NAME")
-AWS_S3_REGION_NAME=os.getenv("AWS_S3_REGION_NAME")
-AWS_S3_FILE_OVERWRITE= json.loads(os.getenv("AWS_S3_FILE_OVERWRITE").lower())
-AWS_DEFAULT_ACL= json.loads(os.getenv("AWS_DEFAULT_ACL"))
-AWS_S3_VERITY=json.loads(os.getenv("AWS_S3_VERITY").lower())
-DEFAULT_FILE_STORAGE=os.getenv("DEFAULT_FILE_STORAGE")
+STORAGES = {
+    # Media file (image) management
+    "default": {
+        "BACKEND": os.getenv(
+            "DEFAULT_FILE_BACKEND_STORAGE"
+        )  # "django.core.files.storage.FileSystemStorage"  # os.getenv("DEFAULT_FILE_BACKEND_STORAGE")
+    },
+    # CSS and JS file management
+    "staticfiles": {
+        "BACKEND": os.getenv(
+            "DEFAULT_FILE_BACKEND_STORAGE"
+        )  # "django.contrib.staticfiles.storage.StaticFilesStorage"  # os.getenv("DEFAULT_FILE_BACKEND_STORAGE")
+    },
+}
